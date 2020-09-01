8fbc01c91c841247db94538827fa85bc9b7ff474
Joshua Spence <josh@joshuaspence.com>
2020-09-01 20:53:50 +1000

    Make JSON schemas compatible with kubeval

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function crd_to_json_schema() {
+  local api_version group input kind version
+
+  echo "Processing ${1}..."
+  input="input/${1}.yaml"
+  curl --silent --show-error "${2}" > "${input}"
+
+  api_version=$(yq read "${input}" apiVersion | cut --delimiter=/ --fields=2)
+  kind=$(yq read "${input}" spec.names.kind | tr '[:upper:]' '[:lower:]')
+  group=$(yq read "${input}" spec.group | cut --delimiter=. --fields=1)
+
+  case "${api_version}" in
+    v1beta1)
+      version=$(yq read "${input}" spec.version)
+      yq read --prettyPrint --tojson "${input}" spec.validation.openAPIV3Schema > "master-standalone/${kind}-${group}-${version}.json"
+      ;;
+
+    v1)
+      for version in $(yq read "${input}" spec.versions.*.name); do
+        yq read --prettyPrint --tojson "${input}" "spec.versions.(name==${version}).schema.openAPIV3Schema" > "master-standalone/${kind}-${group}-${version}.json"
+      done
+      ;;
+
+    *)
+      echo "Unknown API version: ${version}" >&2
+      return 1
+      ;;
+  esac
+}
+
+crd_to_json_schema cert-manager https://raw.githubusercontent.com/jetstack/cert-manager/master/deploy/crds/crd-clusterissuers.yaml
+crd_to_json_schema helm-operator https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
