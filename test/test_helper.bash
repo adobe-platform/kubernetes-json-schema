c5e9a6b68f9e6ccaa03fa732fb25ab30f3e54782
Joshua Spence <josh@joshuaspence.com>
2020-09-18 08:40:03 +1000

    Fix lint

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -4,4 +4,5 @@ function kubeval() {
   command kubeval --additional-schema-locations file://. "$@"
 }
 
+# shellcheck disable=SC2034 disable=SC2154
 readonly FIXTURES="${BATS_TEST_DIRNAME}/$(basename "${BATS_TEST_FILENAME}" .bats)"
